d2e07357e7ca459e52509930c8899d10df07b718
Alana <rgreen.alana@gmail.com>
2026-03-27 15:14:33 -0400

    Preprocessing in progress

--- data_analysis/data_analytics.py
+++ data_analysis/data_analytics.py
@@ -2,5 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+#loaded data
 energy_df = pd.read_csv(r'C:\Users\firea\Desktop\uwu\Intelligent_abnormal_electricity_usage_dataset_REALWORLD.csv')
+#dropped null values
+energy_df.isnull().sum()
+energy_df = energy_df.dropna()
+#dropped useless column
+energy_df = energy_df.drop(columns=['Meter_Id'], errors='ignore')
